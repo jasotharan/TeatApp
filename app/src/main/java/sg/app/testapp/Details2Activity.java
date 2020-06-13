2da15689be436f890b307a4875eee6c1817ddef4
jasotharan <jasotharan1990@gmail.com>
2020-06-13 19:31:56 +0800

    set save and edit - text in details page

--- app/src/main/java/sg/app/testapp/Details2Activity.java
+++ app/src/main/java/sg/app/testapp/Details2Activity.java
@@ -22,7 +22,7 @@ public class Details2Activity extends AppCompatActivity {
     private User selectedUser;
     private Button saveUser;
     private ImageView userImageView;
-    private TextView userLongText;
+    private TextView userLongText,toolbar_edit,toolbar_cancel;
     private EditText userLongEditText;
 
     @Override
@@ -54,6 +54,8 @@ public class Details2Activity extends AppCompatActivity {
     }
 
     private void initUIComponents() {
+        toolbar_edit = findViewById(R.id.toolbar_edit);
+        toolbar_cancel = findViewById(R.id.toolbar_cancel);
         userImageView = findViewById(R.id.user_image_view);
         saveUser = findViewById(R.id.save_user);
         userLongText = findViewById(R.id.user_long_text);
@@ -67,6 +69,8 @@ public class Details2Activity extends AppCompatActivity {
                 saveUser.setVisibility(View.GONE);
                 userLongText.setVisibility(View.VISIBLE);
                 userLongText.setText(userLongEditText.getText());
+                toolbar_edit.setVisibility(View.VISIBLE);
+                toolbar_cancel.setVisibility(View.GONE);
 
             }
         });
@@ -80,11 +84,22 @@ public class Details2Activity extends AppCompatActivity {
 
     public void editUser(View v) {
         // does something very interesting
-        Toast.makeText(mContext, "Edit", Toast.LENGTH_SHORT).show();
+      //  Toast.makeText(mContext, "Edit", Toast.LENGTH_SHORT).show();
         userLongEditText.setVisibility(View.VISIBLE);
         saveUser.setVisibility(View.VISIBLE);
         userLongText.setVisibility(View.GONE);
-        userLongEditText.setText(selectedUser.getTitle());
+        toolbar_edit.setVisibility(View.GONE);
+        toolbar_cancel.setVisibility(View.VISIBLE);
+        userLongEditText.setText(userLongText.getText());
+
+    } public void saveCancel(View v) {
+        // does something very interesting
+       // Toast.makeText(mContext, "cancel", Toast.LENGTH_SHORT).show();
+        userLongEditText.setVisibility(View.GONE);
+        saveUser.setVisibility(View.GONE);
+        userLongText.setVisibility(View.VISIBLE);
+        toolbar_edit.setVisibility(View.VISIBLE);
+        toolbar_cancel.setVisibility(View.GONE);
 
     }
 }
